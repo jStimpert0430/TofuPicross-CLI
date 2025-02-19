99ef9e55f0bd25d0ddff2026943bdb016f5a3c20
Tofu Shop <jStimpert0430@gmail.com>
2025-02-19 18:45:59 -0500

    c

--- src/puzzle.h
+++ src/puzzle.h
@@ -20,7 +20,7 @@ class Puzzle{
 		{false, false, false, true, true, true, true, true, false, false},
 		{false, false, true, true, true, true, true, true, true, false},
 		{false, false, false, false, false, true, false, false, false , false},
-		{true, true, true, true, true, true, true, true, true, true},
+		{true, false, true, true, true, true, true, true, true, true},
 		{false, true, true, true, true, true, true, true, true , false},
 		{false, false, true, true, true, true, true, true, true, false} };
 
